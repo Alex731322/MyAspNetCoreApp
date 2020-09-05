596addda325a7f60f29fec2664e82d81d2c0aa20
Alex <sasha13223@mail.ru>
2020-09-05 21:56:39 +0300

    Добавьте файлы проекта.

---
+++ MyAspNetCoreApp/RoutingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MyAspNetCoreApp
+{
+    public class RoutingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            if(path == "/index")
+            {
+                await context.Response.WriteAsync("Home page");
+
+            }
+            else if(path == "/about")
+            {
+                await context.Response.WriteAsync("About");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+        
+    }
+}
